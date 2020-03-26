ca6f5c9bf1253c8e4c0788bd9e5c646f6f3498ce
Emiel Löhr <e.lohr@avayo.nl>
2020-03-26 16:14:02 +0100

    Add extra optional parameters to alliance_add_invoice and bump version.

--- lib/paynl/api/request/alliance_add_invoice.rb
+++ lib/paynl/api/request/alliance_add_invoice.rb
@@ -2,11 +2,15 @@ module Paynl
   module Api
     class AllianceAddInvoice < Alliance
       MANDATORY_PARAMETERS = [:serviceId, :merchantId, :invoiceId, :amount, :description]
-      OPTIONAL_PARAMETERS = [:invoiceUrl, :makeYesterday]
+      OPTIONAL_PARAMETERS = [:invoiceUrl, :makeYesterday, :extra1, :extra2, :extra3, :merchantServiceId]
 
       # options hash can hold the following:
       #   invoiceUrl - string - A URL pointing to the location of the invoice
       #   makeYesterday - boolean - Wether the transaction should be backdated to yesterday
+      #   extra1 - string - The first free value.
+      #   extra2 - string - The second free value.
+      #   extra3 - string - The third free value.
+      #   merchantServiceId - string - The service id of the merchant to invoice.
       def initialize(serviceId, merchantId, invoiceId, amount, description, options={})
         @params = {
           serviceId: serviceId,
